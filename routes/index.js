8511208faf25ab1404a35ac4412d9be862b1e16f
Adjoa Hackman <adjoahackman93@gmail.com>
2024-09-22 23:02:31 -0400

    trying to figure out my user model and routes and getting a 400 error in insomnia

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const eventRoutes = require('./eventRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/events', eventRoutes);
+router.use('/user', userRoutes);
+
+
 
 
 module.exports = router;
